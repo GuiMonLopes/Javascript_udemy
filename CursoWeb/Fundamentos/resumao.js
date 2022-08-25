5c92b2e8a93610e7eaf1df8cb4093a9542bd5cf3
GuiMonLopes <guilherme.monteiro1@gmail.com>
2022-08-25 17:35:39 -0300

    String e Array

--- CursoWeb/Fundamentos/resumao.js
+++ CursoWeb/Fundamentos/resumao.js
@@ -1,11 +1,42 @@
-console.log(7/0)
+// console.log(7/0)
 
-const string = "guilherme";
-var ss = ""
+// const string = "guilherme";
+// var ss = ""
 
-console.log(string.replace(string.substring(1, string.length).split(/\w/g),"*"));
-console.log(string.substring(1, string.length).split(/\w/g))
-console.log(string.substring(1, string.length))
-console.log(string.substring(1, string.length).split(/\w/g).replace("","*"))
-ss = string.substring(1, string.length).split(/\w/g).replace("","*")
-console.log(ss)
+// console.log(string.substring(1, string.length));
+// console.log(string.split(/\w/g))
+// console.log(string.substring(1, string.length))
+// console.log(string.replace(/e/g,"*"))
+
+let isActive = false
+
+console.log(isActive);
+
+isActive = true
+
+console.log(isActive);
+
+isActive = 1
+
+console.log(!!isActive);
+
+
+console.log("Os verdadeiros....");
+console.log(!!3);
+console.log(!!-1);
+console.log(!!" ");
+console.log(!!"test")
+console.log(!![]);
+console.log(!!{});
+console.log(!!Infinity);
+console.log(!!(isActive = true));
+
+console.log("Os falsos....");
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+console.log(!!(isActive = false));
+
+const valores = []
